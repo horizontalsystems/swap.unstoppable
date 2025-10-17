5fc719a8a3930bc1b309b4d764656e665f9e5970
EA <ealymbaev@gmail.com>
2025-10-17 16:36:06 +0600

    Restore fee calculations for use-balance hook

--- src/hooks/use-balance.ts
+++ src/hooks/use-balance.ts
@@ -2,8 +2,21 @@ import { useQuery } from '@tanstack/react-query'
 import { useAssetFrom } from '@/hooks/use-swap'
 import { useWallets } from '@/hooks/use-wallets'
 import { useInboundAddresses } from '@/hooks/use-inbound-addresses'
-import { BigIntArithmetics } from '@swapkit/core'
+import {
+  AssetValue,
+  BigIntArithmetics,
+  Chain,
+  CosmosChain,
+  CosmosChains,
+  EVMChain,
+  EVMChains,
+  FeeOption,
+  isGasAsset,
+  UTXOChain,
+  UTXOChains
+} from '@swapkit/core'
 import { getSwapKit } from '@/lib/wallets'
+import { estimateTransactionFee } from '@swapkit/toolboxes/cosmos'
 
 type UseBalance = {
   balance?: {
@@ -39,7 +52,7 @@ export const useBalance = (): UseBalance => {
         return null
       }
 
-      let value = new BigIntArithmetics(0)
+      let value = AssetValue.from({ chain: assetFrom.chain, value: 0 })
 
       if ('getBalance' in wallet) {
         const balances = await wallet.getBalance(wallet.address, true)
@@ -50,58 +63,45 @@ export const useBalance = (): UseBalance => {
         }
       }
 
-      const fee = new BigIntArithmetics(0)
+      let fee = new BigIntArithmetics(0)
 
-      // if (baseAsset.isGasAsset && amount > 0n) {
-      //   const inbound = inboundAddresses.find((a: any) => a.chain === assetFrom.chain)
-      //   if (!inbound) {
-      //     return null
-      //   }
-      //
-      //   let estimation: AssetValue | undefined
-      //
-      //   if (CosmosChains.includes(assetFrom.chain as CosmosChain)) {
-      //     const estimate = estimateTransactionFee({ assetValue: baseAsset as any })
-      //     fee = estimate.bigIntValue * BigInt(10 ** (baseAsset.decimal || 0))
-      //   }
-      //
-      //   if (EVMChains.includes(assetFrom.chain as EVMChain)) {
-      //     const wallet = swapkit.getWallet<EVMChain>(selected.provider, selected.network as EVMChain)
-      //     estimation = await wallet?.estimateTransactionFee({
-      //       to: inbound.address,
-      //       from: selected.address,
-      //       value: amountFrom,
-      //       data: '0x',
-      //       chain: assetFrom.chain as EVMChain,
-      //       feeOption: FeeOption.Fast
-      //     })
-      //   }
-      //
-      //   if (UTXOChains.includes(assetFrom.chain as UTXOChain)) {
-      //     const wallet = swapkit.getWallet<UTXOChain>(selected.provider, selected.network as UTXOChain)
-      //     estimation = await wallet?.estimateTransactionFee({
-      //       recipient: inbound.address,
-      //       sender: selected.address,
-      //       assetValue: AssetValue.from({ chain: assetFrom.chain, value: amountFrom }),
-      //       feeOptionKey: FeeOption.Fast
-      //     })
-      //   }
-      //
-      //   if (assetFrom.chain === Chain.Tron) {
-      //     const wallet = swapkit.getWallet<Chain.Tron>(selected.provider, selected.network as Chain.Tron)
-      //     estimation = await wallet?.estimateTransactionFee({
-      //       sender: selected.address,
-      //       recipient: inbound.address,
-      //       assetValue: AssetValue.from({ chain: assetFrom.chain, value: amountFrom }),
-      //       feeOptionKey: FeeOption.Fast
-      //     })
-      //   }
-      //
-      //   if (estimation) {
-      //     const simulationFee = (estimation.bigIntValue * BigInt(1e8)) / BigInt(10 ** (estimation.decimal || 0))
-      //     fee = (simulationFee * 11n) / 10n // surcharge by 10%
-      //   }
-      // }
+      if (isGasAsset({ chain: assetFrom.chain, symbol: assetFrom.metadata.symbol }) && value.gt(0)) {
+        const inbound = inboundAddresses.find((a: any) => a.chain === assetFrom.chain)
+
+        if (!inbound) {
+          return null
+        }
+
+        if (EVMChains.includes(assetFrom.chain as EVMChain)) {
+          const wallet = swapKit.getWallet<EVMChain>(selected.provider, selected.network as EVMChain)
+          fee = await wallet.estimateTransactionFee({
+            to: inbound.address,
+            from: selected.address,
+            value: value.getBaseValue('bigint'),
+            data: '0x',
+            chain: assetFrom.chain as EVMChain,
+            feeOption: FeeOption.Fast
+          })
+        } else if (UTXOChains.includes(assetFrom.chain as UTXOChain)) {
+          const wallet = swapKit.getWallet<UTXOChain>(selected.provider, selected.network as UTXOChain)
+          fee = await wallet.estimateTransactionFee({
+            recipient: inbound.address,
+            sender: selected.address,
+            assetValue: value,
+            feeOptionKey: FeeOption.Fast
+          })
+        } else if (CosmosChains.includes(assetFrom.chain as CosmosChain)) {
+          fee = estimateTransactionFee({ assetValue: value })
+        } else if (assetFrom.chain === Chain.Tron) {
+          const wallet = swapKit.getWallet<Chain.Tron>(selected.provider, selected.network as Chain.Tron)
+          fee = await wallet.estimateTransactionFee({
+            sender: selected.address,
+            recipient: inbound.address,
+            assetValue: value,
+            feeOptionKey: FeeOption.Fast
+          })
+        }
+      }
 
       return {
         total: value,
