3f1cccde3c6f4439ca8817e4dacd7e2452d4bd13
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-11-12 11:58:18 +0600

    Map coingecko ids

--- src/hooks/use-assets.ts
+++ src/hooks/use-assets.ts
@@ -4,39 +4,47 @@ import { Asset } from '@/components/swap/asset'
 
 const PROVIDERS = ['THORCHAIN', 'NEAR', 'ONEINCH']
 
-export const useAssets = (): { assets: Asset[] | undefined; isLoading: boolean } => {
+export const useAssets = (): { assets?: Asset[]; geckoMap?: Map<string, string>; isLoading: boolean } => {
   const { data, isLoading } = useQuery({
     queryKey: ['assets'],
     queryFn: async () => {
-      const lists = await Promise.all(PROVIDERS.map(provider => getTokenList(provider)))
+      const lists = await Promise.all(PROVIDERS.map(getTokenList))
+      const tokens = lists.flatMap(l => l.tokens).filter(t => t.chain)
 
-      const map = lists
-        .map(l => l.tokens)
-        .flat()
-        .filter(t => t.chain)
-        .map(t => ({
-          address: t.address,
-          chain: t.chain,
-          chainId: t.chainId,
-          coingeckoId: t.coingeckoId,
-          decimals: t.decimals,
-          identifier: t.identifier,
-          logoURI: t.logoURI,
-          name: t.name,
-          shortCode: t.shortCode,
-          ticker: t.ticker
-        }))
-        .reduce((acc: Map<string, Asset>, cur: Asset) => {
-          return {
-            ...acc,
-            [`${cur.chain}-${cur.identifier}`.toLowerCase()]: cur
-          }
-        }, new Map<string, Asset>())
+      const assets = new Map<string, Asset>()
+      const geckoMap = new Map<string, string>()
 
-      return Object.values(map)
+      for (const token of tokens) {
+        const key = `${token.chain}-${token.identifier}`.toLowerCase()
+        assets.set(key, {
+          address: token.address,
+          chain: token.chain,
+          chainId: token.chainId,
+          coingeckoId: token.coingeckoId,
+          decimals: token.decimals,
+          identifier: token.identifier,
+          logoURI: token.logoURI,
+          name: token.name,
+          shortCode: token.shortCode,
+          ticker: token.ticker
+        })
+
+        if (token.coingeckoId) {
+          geckoMap.set(token.identifier, token.coingeckoId)
+        }
+      }
+
+      return {
+        assets: Array.from(assets.values()),
+        geckoMap
+      }
     },
     refetchOnMount: false
   })
 
-  return { assets: data, isLoading }
+  return {
+    assets: data?.assets,
+    geckoMap: data?.geckoMap,
+    isLoading
+  }
 }
