d920816fbe8849d71c11f531aaecde2532bae631
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-03-09 12:42:50 +0600

    Show provider icons correctly

--- src/components/swap/swap-provider.tsx
+++ src/components/swap/swap-provider.tsx
@@ -7,33 +7,28 @@ export const SwapProvider = ({ provider }: { provider: ProviderName }) => {
 
   if (provider === 'THORCHAIN' || provider === 'THORCHAIN_STREAMING') {
     title = 'THORChain'
-    icon = 'thorchain'
   } else if (provider === 'MAYACHAIN' || provider === 'MAYACHAIN_STREAMING') {
     title = 'MayaChain'
-    icon = 'mayachain'
   } else if (provider === 'NEAR') {
     title = 'Near'
-    icon = 'near'
   } else if (provider === 'ONEINCH') {
     title = '1inch'
     icon = 'oneinch'
   } else if (provider === 'LETSEXCHANGE') {
     title = 'LetsExchange'
-    icon = 'letsexchange'
   } else if (provider === 'QUICKEX') {
     title = 'QuickEx'
-    icon = 'quickex'
   } else if (provider === 'STEALTHEX') {
     title = 'StealthEX'
-    icon = 'stealthex'
   } else if (provider === 'SWAPUZ') {
     title = 'Swapuz'
-    icon = 'swapuz'
+  } else if (provider === 'EXOLIX') {
+    title = 'Exolix'
   }
 
   return (
     <div className="flex items-center gap-2">
-      <Image src={`/providers/${icon}.svg`} alt="" width="16" height="16" />
+      <Image src={`/providers/${icon.toLowerCase()}.svg`} alt="" width="16" height="16" />
       <span className="text-leah">{title}</span>
     </div>
   )
